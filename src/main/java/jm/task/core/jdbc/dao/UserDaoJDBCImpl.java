117a54753ccbbca98617b47c1cd9809929f3f2fb
Vadim T <denfal@mail.ru>
2021-10-28 19:23:31 +0300

    Строки - в запросе в DaoJDBC

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,46 +8,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    Util util = new Util();
+    private Util util = new Util();
 
     public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
-        String queryCreateUsersTable = "CREATE TABLE IF NOT EXISTS user " +
-                "(id BIGINT NOT NULL AUTO_INCREMENT, " +
-                " Name VARCHAR(45), " +
-                " lastName VARCHAR(45), " +
-                " age tinyint(3), " +
-                " PRIMARY KEY ( id ))";
-
         try (Connection connection = util.getConnection()) {
-            connection.createStatement().executeUpdate(queryCreateUsersTable);
+            connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS user " +
+                    "(id BIGINT NOT NULL AUTO_INCREMENT, " +
+                    " Name VARCHAR(45), " +
+                    " lastName VARCHAR(45), " +
+                    " age tinyint(3), " +
+                    " PRIMARY KEY ( id ))");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
-        String queryDropUsersTable = "DROP TABLE IF EXISTS user";
-
         try (Connection connection = util.getConnection()) {
-            connection.createStatement().executeUpdate(queryDropUsersTable);
+            connection.createStatement().executeUpdate("DROP TABLE IF EXISTS user");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String querySaveUser = "INSERT INTO user (NAME, LASTNAME, AGE) Values (?, ?, ?)";
         Connection connection = null;
-
         try {
             connection = util.getConnection();
             connection.setAutoCommit(false);
 
-            PreparedStatement preparedStatement = connection.prepareStatement(querySaveUser);
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO user (NAME, LASTNAME, AGE) Values (?, ?, ?)");
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
@@ -71,14 +64,12 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        String queryRemoveUserById = "DELETE FROM user (ID) values (?)";
         Connection connection = null;
-
         try {
             connection = util.getConnection();
             connection.setAutoCommit(false);
 
-            PreparedStatement preparedStatement = connection.prepareStatement(queryRemoveUserById);
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM user (ID) values (?)");
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
             connection.commit();
@@ -101,10 +92,9 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> list = new ArrayList<>();
-        String query = "select * from user";
 
         try (Connection connection = util.getConnection()) {
-            ResultSet resultSet = connection.createStatement().executeQuery(query);
+            ResultSet resultSet = connection.createStatement().executeQuery("select * from user");
             while (resultSet.next()) {
                 User user = new User();
                 user.setId((long) resultSet.getInt(1));
@@ -120,14 +110,12 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        String queryСleanUsersTable = "DELETE FROM user";
         Connection connection = null;
-
         try {
             connection = util.getConnection();
             connection.setAutoCommit(false);
 
-            connection.createStatement().executeUpdate(queryСleanUsersTable);
+            connection.createStatement().executeUpdate("DELETE FROM user");
             connection.commit();
             connection.close();
         } catch (SQLException e) {
